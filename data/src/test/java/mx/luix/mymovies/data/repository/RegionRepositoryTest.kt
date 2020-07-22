dc379e2aa2559b8220015bebfb00781f1d548859
LuiX <luis.a.vivero@gmail.com>
2020-07-22 16:42:38 -0700

    Add a test in RegionRepositoryTest to verify that returns default region when coarse permission is not granted

--- data/src/test/java/mx/luix/mymovies/data/repository/RegionRepositoryTest.kt
+++ data/src/test/java/mx/luix/mymovies/data/repository/RegionRepositoryTest.kt
@@ -1,7 +1,12 @@
 package mx.luix.mymovies.data.repository
 
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
+import mx.luix.mymovies.data.repository.PermissionChecker.Permission.COARSE_LOCATION
 import mx.luix.mymovies.data.source.LocationDataSource
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
@@ -20,5 +25,17 @@ class RegionRepositoryTest {
     @Before
     fun setUp() {
         regionRepository = RegionRepository(locationDataSource, permissionChecker)
+    }
+
+    @Test
+    fun `returns default when coarse permission not granted`() {
+        runBlocking {
+
+            whenever(permissionChecker.check(COARSE_LOCATION)).thenReturn(false)
+
+            val region = regionRepository.findLastRegion()
+
+            assertEquals(RegionRepository.DEFAULT_REGION, region)
+        }
     }
 }
