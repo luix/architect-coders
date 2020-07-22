f485be471716d0944453c5ca823919e0f099c398
LuiX <luis.a.vivero@gmail.com>
2020-07-22 16:20:16 -0700

    Implement the MoviesRepository class

--- data/src/main/java/mx/luix/mymovies/data/repository/MoviesRepository.kt
+++ data/src/main/java/mx/luix/mymovies/data/repository/MoviesRepository.kt
@@ -1,2 +1,28 @@
 package mx.luix.mymovies.data.repository
 
+import mx.luix.mymovies.data.source.LocalDataSource
+import mx.luix.mymovies.data.source.RemoteDataSource
+import mx.luix.mymovies.domain.Movie
+
+class MoviesRepository(
+    private val localDataSource: LocalDataSource,
+    private val remoteDataSource: RemoteDataSource,
+    private val regionRepository: RegionRepository,
+    private val apiKey: String
+) {
+
+    suspend fun getPopularMovies(): List<Movie> {
+
+        if (localDataSource.isEmpty()) {
+            val movies =
+                remoteDataSource.getPopularMovies(apiKey, regionRepository.findLastRegion())
+            localDataSource.saveMovies(movies)
+        }
+
+        return localDataSource.getPopularMovies()
+    }
+
+    suspend fun findById(id: Int): Movie = localDataSource.findById(id)
+
+    suspend fun update(movie: Movie) = localDataSource.update(movie)
+}
