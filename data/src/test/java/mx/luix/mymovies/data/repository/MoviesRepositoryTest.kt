9f89246234c448d3b835da31d2a463550ef06ef9
LuiX <luis.a.vivero@gmail.com>
2020-07-22 16:55:28 -0700

    Add a test in MoviesRepositoryTest class to verify that getPopularMovies saves remote data to local

--- data/src/test/java/mx/luix/mymovies/data/repository/MoviesRepositoryTest.kt
+++ data/src/test/java/mx/luix/mymovies/data/repository/MoviesRepositoryTest.kt
@@ -1,5 +1,7 @@
 package mx.luix.mymovies.data.repository
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.runBlocking
 import mx.luix.mymovies.data.source.LocalDataSource
@@ -46,6 +48,21 @@ class MoviesRepositoryTest {
             val result = moviesRepository.getPopularMovies()
 
             assertEquals(localMovies, result)
+        }
+    }
+
+    @Test
+    fun `getPopularMovies saves remote data to local`() {
+        runBlocking {
+
+            val remoteMovies = listOf(mockedMovie.copy(2))
+            whenever(localDataSource.isEmpty()).thenReturn(true)
+            whenever(remoteDataSource.getPopularMovies(any(), any())).thenReturn(remoteMovies)
+            whenever(regionRepository.findLastRegion()).thenReturn("US")
+
+            moviesRepository.getPopularMovies()
+
+            verify(localDataSource).saveMovies(remoteMovies)
         }
     }
 }
