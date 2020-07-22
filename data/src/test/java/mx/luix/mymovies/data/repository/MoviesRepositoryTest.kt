5fbeda7395d2d58aad9e0380426eb78467c5a655
LuiX <luis.a.vivero@gmail.com>
2020-07-22 16:52:08 -0700

    Add a test in MoviesRepositoryTest class to verify that getPopularMovies gets from local data source first

--- data/src/test/java/mx/luix/mymovies/data/repository/MoviesRepositoryTest.kt
+++ data/src/test/java/mx/luix/mymovies/data/repository/MoviesRepositoryTest.kt
@@ -1,9 +1,14 @@
 package mx.luix.mymovies.data.repository
 
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
 import mx.luix.mymovies.data.source.LocalDataSource
 import mx.luix.mymovies.data.source.LocationDataSource
 import mx.luix.mymovies.data.source.RemoteDataSource
+import mx.luix.mymovies.testshared.mockedMovie
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
@@ -28,5 +33,19 @@ class MoviesRepositoryTest {
     fun setUp() {
         moviesRepository =
             MoviesRepository(localDataSource, remoteDataSource, regionRepository, apiKey)
+    }
+
+    @Test
+    fun `getPopularMovies gets from local data source first`() {
+        runBlocking {
+
+            val localMovies = listOf(mockedMovie.copy(1))
+            whenever(localDataSource.isEmpty()).thenReturn(false)
+            whenever(localDataSource.getPopularMovies()).thenReturn(localMovies)
+
+            val result = moviesRepository.getPopularMovies()
+
+            assertEquals(localMovies, result)
+        }
     }
 }
