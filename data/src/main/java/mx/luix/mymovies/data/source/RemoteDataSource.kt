18c5c0f206cc435b71918d6a47f8c41bd2778170
LuiX <luis.a.vivero@gmail.com>
2020-07-22 16:19:12 -0700

    Add a RemoteDataSource interface

--- data/src/main/java/mx/luix/mymovies/data/source/RemoteDataSource.kt
+++ data/src/main/java/mx/luix/mymovies/data/source/RemoteDataSource.kt
@@ -1,4 +1,7 @@
 package mx.luix.mymovies.data.source
 
+import mx.luix.mymovies.domain.Movie
+
 interface RemoteDataSource {
+    suspend fun getPopularMovies(apiKey: String, region: String): List<Movie>
 }
