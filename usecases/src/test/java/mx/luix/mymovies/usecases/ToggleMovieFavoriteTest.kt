ec5d7195c4d1b0fcea5b9bcf2f1f36669160fd56
LuiX <luis.a.vivero@gmail.com>
2020-07-23 20:47:19 -0700

    Create ToggleMovieFavoriteTest class

--- usecases/src/test/java/mx/luix/mymovies/usecases/ToggleMovieFavoriteTest.kt
+++ usecases/src/test/java/mx/luix/mymovies/usecases/ToggleMovieFavoriteTest.kt
@@ -1,4 +1,21 @@
 package mx.luix.mymovies.usecases
 
+import mx.luix.mymovies.data.repository.MoviesRepository
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
 class ToggleMovieFavoriteTest {
+
+    @Mock
+    lateinit var moviesRepository: MoviesRepository
+
+    lateinit var toggleMovieFavorite: ToggleMovieFavorite
+
+    @Before
+    fun setUp() {
+        toggleMovieFavorite = ToggleMovieFavorite(moviesRepository)
+    }
 }
