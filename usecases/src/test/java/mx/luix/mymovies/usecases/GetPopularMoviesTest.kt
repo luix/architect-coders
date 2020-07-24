2445e9155b20ef3cc51a72790dd58bcfb2d1fd7c
LuiX <luis.a.vivero@gmail.com>
2020-07-23 21:01:07 -0700

    Add a test in GetPopularMoviesTest to verify that invoke calls movies repository

--- usecases/src/test/java/mx/luix/mymovies/usecases/GetPopularMoviesTest.kt
+++ usecases/src/test/java/mx/luix/mymovies/usecases/GetPopularMoviesTest.kt
@@ -1,7 +1,12 @@
 package mx.luix.mymovies.usecases
 
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
 import mx.luix.mymovies.data.repository.MoviesRepository
+import mx.luix.mymovies.testshared.mockedMovie
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
@@ -17,5 +22,18 @@ class GetPopularMoviesTest {
     @Before
     fun setUp() {
         getPopularMovies = GetPopularMovies(moviesRepository)
+    }
+
+    @Test
+    fun `invoke calls movies repository`() {
+        runBlocking {
+
+            val movies = listOf(mockedMovie.copy(id = 1))
+            whenever(moviesRepository.getPopularMovies()).thenReturn(movies)
+
+            val result = getPopularMovies.invoke()
+
+            Assert.assertEquals(movies, result)
+        }
     }
 }
