ed673e621977c4aae72c2363031de558e71bad39
LuiX <luis.a.vivero@gmail.com>
2020-07-23 20:37:45 -0700

    Implement GetPopularMovies use case class

--- usecases/src/main/java/mx/luix/mymovies/usecases/GetPopularMovies.kt
+++ usecases/src/main/java/mx/luix/mymovies/usecases/GetPopularMovies.kt
@@ -1,4 +1,9 @@
 package mx.luix.mymovies.usecases
 
-class GetPopularMovies {
+import mx.luix.mymovies.data.repository.MoviesRepository
+import mx.luix.mymovies.domain.Movie
+
+class GetPopularMovies(private val moviesRepository: MoviesRepository) {
+
+    suspend fun invoke(): List<Movie> = moviesRepository.getPopularMovies()
 }
