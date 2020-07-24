c01b9e921e29d0292ebd1f97071e2e4fa13798b5
LuiX <luis.a.vivero@gmail.com>
2020-07-23 21:04:19 -0700

    Add a test in FindMovieByIdTest to verify that invoke calls movies repository

--- usecases/src/test/java/mx/luix/mymovies/usecases/FindMovieByIdTest.kt
+++ usecases/src/test/java/mx/luix/mymovies/usecases/FindMovieByIdTest.kt
@@ -1,7 +1,12 @@
 package mx.luix.mymovies.usecases
 
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
 import mx.luix.mymovies.data.repository.MoviesRepository
+import mx.luix.mymovies.testshared.mockedMovie
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
@@ -17,5 +22,18 @@ class FindMovieByIdTest {
     @Before
     fun setUp() {
         findMovieById = FindMovieById(moviesRepository)
+    }
+
+    @Test
+    fun `invoke calls movies repository`() {
+        runBlocking {
+
+            val movie = mockedMovie.copy(id = 1)
+            whenever(moviesRepository.findById(1)).thenReturn(movie)
+
+            val result = findMovieById.invoke(1)
+
+            assertEquals(movie, result)
+        }
     }
 }
