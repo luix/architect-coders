c15a60756fcab09669ac17800f94bddab2fef378
LuiX <luis.a.vivero@gmail.com>
2020-07-23 20:52:18 -0700

    Add a test in ToggleMovieFavoriteTest to verify that invoke calls movies repository

--- usecases/src/test/java/mx/luix/mymovies/usecases/ToggleMovieFavoriteTest.kt
+++ usecases/src/test/java/mx/luix/mymovies/usecases/ToggleMovieFavoriteTest.kt
@@ -1,7 +1,11 @@
 package mx.luix.mymovies.usecases
 
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.runBlocking
 import mx.luix.mymovies.data.repository.MoviesRepository
+import mx.luix.mymovies.testshared.mockedMovie
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
@@ -17,5 +21,17 @@ class ToggleMovieFavoriteTest {
     @Before
     fun setUp() {
         toggleMovieFavorite = ToggleMovieFavorite(moviesRepository)
+    }
+
+    @Test
+    fun `invoke calls movies repository`() {
+        runBlocking {
+
+            val movie = mockedMovie.copy(id = 1)
+
+            val result = toggleMovieFavorite.invoke(movie)
+
+            verify(moviesRepository).update(result)
+        }
     }
 }
