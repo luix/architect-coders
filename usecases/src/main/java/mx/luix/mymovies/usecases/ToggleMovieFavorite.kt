f7b58b2f2331d9c60335ab1402d3349e47c81bc0
LuiX <luis.a.vivero@gmail.com>
2020-07-23 20:38:07 -0700

    Implement ToggleMovieFavorite use case class

--- usecases/src/main/java/mx/luix/mymovies/usecases/ToggleMovieFavorite.kt
+++ usecases/src/main/java/mx/luix/mymovies/usecases/ToggleMovieFavorite.kt
@@ -1,4 +1,11 @@
 package mx.luix.mymovies.usecases
 
-class ToggleMovieFavorite {
+import mx.luix.mymovies.data.repository.MoviesRepository
+import mx.luix.mymovies.domain.Movie
+
+class ToggleMovieFavorite(private val moviesRepository: MoviesRepository) {
+
+    suspend fun invoke(movie: Movie): Movie = with(movie) {
+        copy(favorite = !favorite).also { moviesRepository.update(it) }
+    }
 }
