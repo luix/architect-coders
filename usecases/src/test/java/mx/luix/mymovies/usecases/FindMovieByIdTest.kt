4f8a7e190362c98c4abeb8e6022c754c13cf065e
LuiX <luis.a.vivero@gmail.com>
2020-07-23 20:42:43 -0700

    Create FindMovieByIdTest class

---
+++ usecases/src/test/java/mx/luix/mymovies/usecases/FindMovieByIdTest.kt
@@ -0,0 +1,21 @@
+package mx.luix.mymovies.usecases
+
+import mx.luix.mymovies.data.repository.MoviesRepository
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class FindMovieByIdTest {
+
+    @Mock
+    lateinit var moviesRepository: MoviesRepository
+
+    lateinit var findMovieById: FindMovieById
+
+    @Before
+    fun setup() {
+        findMovieById = FindMovieById(moviesRepository)
+    }
+}
