b34afb2d3d0bce0a65436c174b67b0b0f7a5714a
LuiX <luis.a.vivero@gmail.com>
2020-07-25 19:51:30 -0700

    Create and implement a TheMovieDb class that uses OkHttpClient and a Retrofit service

--- app/src/main/java/mx/luix/mymovies/data/server/TheMovieDb.kt
+++ app/src/main/java/mx/luix/mymovies/data/server/TheMovieDb.kt
@@ -1,4 +1,21 @@
 package mx.luix.mymovies.data.server
 
-class TheMovieDb {
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class TheMovieDb(baseUrl: String) {
+
+    val okHttpClient = HttpLoggingInterceptor().run {
+        level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder().addInterceptor(this).build()
+    }
+
+    val service: TheMovieDbService = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .run { create(TheMovieDbService::class.java) }
 }
