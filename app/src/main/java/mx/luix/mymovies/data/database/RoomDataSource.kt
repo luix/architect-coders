7b77af5a74f5c48208c314801649849f51815a72
LuiX <luis.a.vivero@gmail.com>
2020-07-25 19:45:44 -0700

    Create a RoomDataSource class that implements a LocalDataSource

--- app/src/main/java/mx/luix/mymovies/data/database/RoomDataSource.kt
+++ app/src/main/java/mx/luix/mymovies/data/database/RoomDataSource.kt
@@ -1,4 +1,32 @@
 package mx.luix.mymovies.data.database
 
-class RoomDataSource {
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import mx.luix.mymovies.data.source.LocalDataSource
+import mx.luix.mymovies.domain.Movie
+import toDomainMovie
+import toRoomMovie
+
+class RoomDataSource(db: MovieDatabase) : LocalDataSource {
+
+    private val movieDao = db.movieDao()
+
+    override suspend fun isEmpty(): Boolean =
+        withContext(Dispatchers.IO) { movieDao.movieCount() <= 0 }
+
+    override suspend fun saveMovies(movies: List<Movie>) {
+        withContext(Dispatchers.IO) { movieDao.insertMovies(movies.map { it.toRoomMovie() }) }
+    }
+
+    override suspend fun getPopularMovies(): List<Movie> = withContext(Dispatchers.IO) {
+        movieDao.getAll().map { it.toDomainMovie() }
+    }
+
+    override suspend fun findById(id: Int): Movie = withContext(Dispatchers.IO) {
+        movieDao.findById(id).toDomainMovie()
+    }
+
+    override suspend fun update(movie: Movie) {
+        withContext(Dispatchers.IO) { movieDao.updateMovie(movie.toRoomMovie()) }
+    }
 }
