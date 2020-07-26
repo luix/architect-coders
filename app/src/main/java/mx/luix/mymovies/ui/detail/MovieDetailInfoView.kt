6ff7ad6de5df5e3c96d1e053e7830b12310694fb
LuiX <luis.a.vivero@gmail.com>
2020-07-26 14:46:32 -0700

    Create a MovieDetailInfoView class that extends from AppCompatTextView

--- app/src/main/java/mx/luix/mymovies/ui/detail/MovieDetailInfoView.kt
+++ app/src/main/java/mx/luix/mymovies/ui/detail/MovieDetailInfoView.kt
@@ -1,4 +1,35 @@
 package mx.luix.mymovies.ui.detail
 
-class MovieDetailInfoView {
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import mx.luix.mymovies.domain.Movie
+
+class MovieDetailInfoView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    fun setMovie(movie: Movie) = with(movie) {
+        text = buildSpannedString {
+
+            bold { append("Original language: ") }
+            appendln(originalLanguage)
+
+            bold { append("Original title: ") }
+            appendln(originalTitle)
+
+            bold { append("Release date: ") }
+            appendln(releaseDate)
+
+            bold { append("Popularity: ") }
+            appendln(popularity.toString())
+
+            bold { append("Vote Average: ") }
+            appendln(voteAverage.toString())
+        }
+    }
 }
