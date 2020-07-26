a44ba98b3b00b626a9c67386bfecd5f2a6e9beac
LuiX <luis.a.vivero@gmail.com>
2020-07-25 19:44:42 -0700

    Create a MovieDatabase abstract class

--- app/src/main/java/mx/luix/mymovies/data/database/MovieDatabase.kt
+++ app/src/main/java/mx/luix/mymovies/data/database/MovieDatabase.kt
@@ -1,4 +1,20 @@
 package mx.luix.mymovies.data.database
 
-class MovieDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Movie::class], version = 1)
+abstract class MovieDatabase : RoomDatabase() {
+
+    companion object {
+        fun build(context: Context) = Room.databaseBuilder(
+            context,
+            MovieDatabase::class.java,
+            "movie-db"
+        ).build()
+    }
+
+    abstract fun movieDao(): MovieDao
 }
