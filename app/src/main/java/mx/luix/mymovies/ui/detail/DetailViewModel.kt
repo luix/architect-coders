8895ac6bc3be66de404491b502c3d2cfacd0f122
LuiX <luis.a.vivero@gmail.com>
2020-07-26 14:47:07 -0700

    Create and implement a DetailViewModel class

--- app/src/main/java/mx/luix/mymovies/ui/detail/DetailViewModel.kt
+++ app/src/main/java/mx/luix/mymovies/ui/detail/DetailViewModel.kt
@@ -1,4 +1,38 @@
 package mx.luix.mymovies.ui.detail
 
-class DetailViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+import mx.luix.mymovies.domain.Movie
+import mx.luix.mymovies.ui.common.ScopedViewModel
+import mx.luix.mymovies.usecases.FindMovieById
+import mx.luix.mymovies.usecases.ToggleMovieFavorite
+
+class DetailViewModel(
+    private val movieId: Int,
+    private val findMovieById: FindMovieById,
+    private val toggleMovieFavorite: ToggleMovieFavorite,
+    override val uiDispatcher: CoroutineDispatcher
+) :
+    ScopedViewModel(uiDispatcher) {
+
+    data class UiModel(val movie: Movie)
+
+    private val _model = MutableLiveData<UiModel>()
+    val model: LiveData<UiModel>
+        get() {
+            if (_model.value == null) findMovie()
+            return _model
+        }
+
+    private fun findMovie() = launch {
+        _model.value = UiModel(findMovieById.invoke(movieId))
+    }
+
+    fun onFavoriteClicked() = launch {
+        _model.value?.movie?.let {
+            _model.value = UiModel(toggleMovieFavorite.invoke(it))
+        }
+    }
 }
