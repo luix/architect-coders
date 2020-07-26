7b4d8256d615d93a28ab0b0b14c5d3646a5f5f12
LuiX <luis.a.vivero@gmail.com>
2020-07-25 19:49:59 -0700

    Create a TheMovieDbService interface

--- app/src/main/java/mx/luix/mymovies/data/server/TheMovieDbService.kt
+++ app/src/main/java/mx/luix/mymovies/data/server/TheMovieDbService.kt
@@ -1,4 +1,12 @@
 package mx.luix.mymovies.data.server
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface TheMovieDbService {
+    @GET("discover/movie?sort_by=popularity.desc")
+    suspend fun listPopularMoviesAsync(
+        @Query("api_key") apiKey: String,
+        @Query("region") region: String
+    ): MovieDbResult
 }
