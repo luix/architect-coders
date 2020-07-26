0ce7aa1ccfc4448e48c907c4af0bb0aeca9a1508
LuiX <luis.a.vivero@gmail.com>
2020-07-25 19:52:55 -0700

    Create a TheMovieDbDataSource that overrides getPopularMovies from RemoteDataSource

--- app/src/main/java/mx/luix/mymovies/data/server/TheMovieDbDataSource.kt
+++ app/src/main/java/mx/luix/mymovies/data/server/TheMovieDbDataSource.kt
@@ -1,4 +1,14 @@
 package mx.luix.mymovies.data.server
 
-class TheMovieDbDataSource {
+import mx.luix.mymovies.data.source.RemoteDataSource
+import mx.luix.mymovies.domain.Movie
+import toDomainMovie
+
+class TheMovieDbDataSource(private val theMovieDb: TheMovieDb) : RemoteDataSource {
+
+    override suspend fun getPopularMovies(apiKey: String, region: String): List<Movie> =
+        theMovieDb.service
+            .listPopularMoviesAsync(apiKey, region)
+            .results
+            .map { it.toDomainMovie() }
 }
