c4c64a0d11d9707cd5ec46b3d528ebc9b0969da8
LuiX <luis.a.vivero@gmail.com>
2020-07-25 20:05:26 -0700

    Implement a PlayServicesLocationDataSource class that overrides the findLastRegion() function from LocationDataSource

--- app/src/main/java/mx/luix/mymovies/data/PlayServicesLocationDataSource.kt
+++ app/src/main/java/mx/luix/mymovies/data/PlayServicesLocationDataSource.kt
@@ -1,4 +1,32 @@
 package mx.luix.mymovies.data
 
-class PlayServicesLocationDataSource {
+import android.annotation.SuppressLint
+import android.app.Application
+import android.location.Geocoder
+import android.location.Location
+import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+import mx.luix.mymovies.data.source.LocationDataSource
+
+class PlayServicesLocationDataSource(application: Application) : LocationDataSource {
+
+    private val geocoder = Geocoder(application)
+    private val fusedLocationClient = LocationServices.getFusedLocationProviderClient(application)
+
+    @SuppressLint("MissingPermission")
+    override suspend fun findLastRegion(): String? =
+        suspendCancellableCoroutine { continuation ->
+            fusedLocationClient.lastLocation
+                .addOnCompleteListener {
+                    continuation.resume(it.result.toRegion())
+                }
+        }
+
+    private fun Location?.toRegion(): String? {
+        val addresses = this?.let {
+            geocoder.getFromLocation(latitude, longitude, 1)
+        }
+        return addresses?.firstOrNull()?.countryCode
+    }
 }
