f9ca4a059fe6aaecdb38f9f0ec1e3c3babee4318
LuiX <luis.a.vivero@gmail.com>
2020-07-26 14:47:21 -0700

    Create and implement a DetailActivity class

--- app/src/main/java/mx/luix/mymovies/ui/detail/DetailActivity.kt
+++ app/src/main/java/mx/luix/mymovies/ui/detail/DetailActivity.kt
@@ -1,4 +1,44 @@
 package mx.luix.mymovies.ui.detail
 
-class DetailActivity {
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_detail.*
+import mx.luix.mymovies.R
+import mx.luix.mymovies.ui.common.loadUrl
+import org.koin.androidx.scope.lifecycleScope
+import org.koin.androidx.viewmodel.scope.viewModel
+import org.koin.core.parameter.parametersOf
+
+class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val MOVIE = "DetailActivity:movie"
+    }
+
+    private val viewModel: DetailViewModel by lifecycleScope.viewModel(this) {
+        parametersOf(intent.getIntExtra(MOVIE, -1))
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        viewModel.model.observe(this, Observer(::updataUi))
+
+        movieDetailFavorite.setOnClickListener { viewModel.onFavoriteClicked() }
+    }
+
+    private fun updataUi(model: DetailViewModel.UiModel) = with(model.movie) {
+        movieDetailToolbar.title = title
+        movieDetailImage.loadUrl("https://image.tmdb.org/t/p/w780$backdropPath")
+        movieDetailSummary.text = overview
+        movieDetailInfo.setMovie(this)
+
+        val icon = if (favorite) R.drawable.ic_favorite_on else R.drawable.ic_favorite_off
+        movieDetailFavorite.setImageDrawable(getDrawable(icon))
+    }
 }
