1889474124330f8454b36e5b16c08e93df1e98da
LuiX <luis.a.vivero@gmail.com>
2020-07-26 22:30:26 -0700

    Create and implement a MainViewModel class

--- app/src/main/java/mx/luix/mymovies/ui/main/MainViewModel.kt
+++ app/src/main/java/mx/luix/mymovies/ui/main/MainViewModel.kt
@@ -1,4 +1,54 @@
 package mx.luix.mymovies.ui.main
 
-class MainViewModel {
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+import mx.luix.mymovies.domain.Movie
+import mx.luix.mymovies.ui.common.ScopedViewModel
+import mx.luix.mymovies.usecases.GetPopularMovies
+
+class MainViewModel(
+    private val getPopularMovies: GetPopularMovies,
+    uiDispatcher: CoroutineDispatcher
+) : ScopedViewModel(uiDispatcher) {
+
+    private val _model = MutableLiveData<UiModel>()
+    val model: LiveData<UiModel>
+        get() {
+            if (_model.value == null) refresh()
+            return _model
+        }
+
+    sealed class UiModel {
+        object Loading : UiModel()
+        data class Content(val movies: List<Movie>) : UiModel()
+        data class Navigation(val movie: Movie) : UiModel()
+        object RequestLocationPermission : UiModel()
+    }
+
+    init {
+        initScope()
+    }
+
+    private fun refresh() {
+        _model.value = UiModel.RequestLocationPermission
+    }
+
+    fun onCoarsePermissionRequested() {
+        launch {
+            _model.value = UiModel.Loading
+            _model.value = UiModel.Content(getPopularMovies.invoke())
+        }
+    }
+
+    fun onMovieClicked(movie: Movie) {
+        _model.value = UiModel.Navigation(movie)
+    }
+
+    override fun onCleared() {
+        destroyScope()
+        super.onCleared()
+    }
 }
