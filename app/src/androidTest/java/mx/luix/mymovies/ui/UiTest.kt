ef54bf1d0003952433eaa3f13d11654daaff8e68
LuiX <luis.a.vivero@gmail.com>
2020-07-28 19:59:41 -0700

    Create and implement a UiTests class

--- app/src/androidTest/java/mx/luix/mymovies/ui/UiTest.kt
+++ app/src/androidTest/java/mx/luix/mymovies/ui/UiTest.kt
@@ -1,2 +1,63 @@
 package mx.luix.mymovies.ui
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
+import com.jakewharton.espresso.OkHttp3IdlingResource
+import kotlinx.android.synthetic.main.activity_main.view.*
+import mx.luix.mymovies.R
+import mx.luix.mymovies.data.server.TheMovieDb
+import mx.luix.mymovies.ui.main.MainActivity
+import mx.luix.mymovies.utils.fromJson
+import okhttp3.mockwebserver.MockResponse
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.koin.test.KoinTest
+import org.koin.test.get
+
+class UiTest : KoinTest {
+
+    @get:Rule
+    val mockWebServerRule = MockWebServerRule()
+
+    @get:Rule
+    val activityTestRule = ActivityTestRule(MainActivity::class.java, false, false)
+
+    @get:Rule
+    val grantPermissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(
+            "android.permission.ACCESS_COARSE_LOCATION"
+        )
+
+    @Before
+    fun setUp() {
+        mockWebServerRule.server.enqueue(
+            MockResponse().fromJson("popularmovies.json")
+        )
+
+        val resource = OkHttp3IdlingResource.create("OkHttp", get<TheMovieDb>().okHttpClient)
+        IdlingRegistry.getInstance().register(resource)
+    }
+
+    @Test
+    fun clickMovieNavigatesToDetail() {
+        activityTestRule.launchActivity(null)
+
+        onView(withId(R.id.recycler)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                4,
+                click()
+            )
+        )
+
+        onView(withId(R.id.movieDetailToolbar))
+            .check(matches(hasDescendant(withText("Spider-Man: Far from Home"))))
+    }
+}
