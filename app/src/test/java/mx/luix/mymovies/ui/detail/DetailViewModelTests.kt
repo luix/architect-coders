b80e79c54ba676e5f51254f0f94e2768caae73f4
LuiX <luis.a.vivero@gmail.com>
2020-07-28 19:57:52 -0700

    Create a DetailViewModelTests class

--- app/src/test/java/mx/luix/mymovies/ui/detail/DetailViewModelTests.kt
+++ app/src/test/java/mx/luix/mymovies/ui/detail/DetailViewModelTests.kt
@@ -1,2 +1,38 @@
 package mx.luix.mymovies.ui.detail
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import kotlinx.coroutines.Dispatchers
+import mx.luix.mymovies.usecases.FindMovieById
+import mx.luix.mymovies.usecases.ToggleMovieFavorite
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.koin.core.context.startKoin
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class DetailViewModelTests {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var findMovieById: FindMovieById
+
+    @Mock
+    lateinit var toggleMovieFavorite: ToggleMovieFavorite
+
+    @Mock
+    lateinit var observer: Observer<DetailViewModel.UiModel>
+
+    private lateinit var vm: DetailViewModel
+
+    @Before
+    fun setUp() {
+        vm = DetailViewModel(1, findMovieById, toggleMovieFavorite, Dispatchers.Unconfined)
+    }
+}
