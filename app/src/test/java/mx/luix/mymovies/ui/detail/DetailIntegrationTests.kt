9651aa4133cf8e0628e50cf5779ba5d8a28511cb
LuiX <luis.a.vivero@gmail.com>
2020-07-28 19:52:47 -0700

    Create a DetailIntegrationTests class

--- app/src/test/java/mx/luix/mymovies/ui/detail/DetailIntegrationTests.kt
+++ app/src/test/java/mx/luix/mymovies/ui/detail/DetailIntegrationTests.kt
@@ -1,2 +1,47 @@
 package mx.luix.mymovies.ui.detail
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import mx.luix.mymovies.data.source.LocalDataSource
+import mx.luix.mymovies.ui.FakeLocalDataSource
+import mx.luix.mymovies.ui.defaultFakeMovies
+import mx.luix.mymovies.ui.initMockedDi
+import mx.luix.mymovies.usecases.FindMovieById
+import mx.luix.mymovies.usecases.ToggleMovieFavorite
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.koin.core.parameter.parametersOf
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class DetailIntegrationTests : AutoCloseKoinTest() {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var observer: Observer<DetailViewModel.UiModel>
+
+    private lateinit var vm: DetailViewModel
+    private lateinit var localDataSource: FakeLocalDataSource
+
+    @Before
+    fun setUp() {
+        val vmModule = module {
+            factory { (id: Int) -> DetailViewModel(id, get(), get(), get()) }
+            factory { FindMovieById(get()) }
+            factory { ToggleMovieFavorite(get()) }
+        }
+
+        initMockedDi(vmModule)
+        vm = get { parametersOf(1) }
+
+        localDataSource = get<LocalDataSource>() as FakeLocalDataSource
+        localDataSource.movies = defaultFakeMovies
+    }
+}
