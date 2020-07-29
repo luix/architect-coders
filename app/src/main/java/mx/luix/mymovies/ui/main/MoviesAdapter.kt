5f2e31ff8236c6e2dd24c70ccb22fd83ef2c48ca
LuiX <luis.a.vivero@gmail.com>
2020-07-28 20:00:24 -0700

    Create an implement a MoviesAdapter class

--- app/src/main/java/mx/luix/mymovies/ui/main/MoviesAdapter.kt
+++ app/src/main/java/mx/luix/mymovies/ui/main/MoviesAdapter.kt
@@ -1,4 +1,40 @@
 package mx.luix.mymovies.ui.main
 
-class MoviesAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.view_movie.view.*
+import mx.luix.mymovies.R
+import mx.luix.mymovies.domain.Movie
+import mx.luix.mymovies.ui.common.basicDiffUtil
+import mx.luix.mymovies.ui.common.inflate
+import mx.luix.mymovies.ui.common.loadUrl
+
+class MoviesAdapter(private val listener: (Movie) -> Unit) :
+    RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
+
+    var movies: List<Movie> by basicDiffUtil(
+        emptyList(),
+        areItemsTheSame = { old, new -> old.id == new.id }
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = parent.inflate(R.layout.view_movie, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.bind(movie)
+        holder.itemView.setOnClickListener { listener(movie) }
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(movie: Movie) {
+            itemView.movieTitle.text = movie.title
+            itemView.movieCover.loadUrl("https://image.tmdb.org/t/p/w185/${movie.posterPath}")
+        }
+    }
 }
