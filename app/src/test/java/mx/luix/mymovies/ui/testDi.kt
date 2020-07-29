945bd3921caca9c11444de1318a339c80e98ab37
LuiX <luis.a.vivero@gmail.com>
2020-07-28 19:25:53 -0700

    Create a FakeRemoteDataSource class to be used for dependency injection tests

--- app/src/test/java/mx/luix/mymovies/ui/testDi.kt
+++ app/src/test/java/mx/luix/mymovies/ui/testDi.kt
@@ -1,11 +1,20 @@
 package mx.luix.mymovies.ui
 
+import com.nhaarman.mockitokotlin2.mock
 import mx.luix.mymovies.data.source.LocalDataSource
+import mx.luix.mymovies.data.source.RemoteDataSource
 import mx.luix.mymovies.domain.Movie
+import mx.luix.mymovies.testshared.mockedMovie
 import org.koin.core.context.startKoin
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
+val defaultFakeMovies = listOf(
+    mockedMovie.copy(1),
+    mockedMovie.copy(2),
+    mockedMovie.copy(3),
+    mockedMovie.copy(4)
+)
 
 class FakeLocalDataSource : LocalDataSource {
 
@@ -24,5 +33,12 @@ class FakeLocalDataSource : LocalDataSource {
     override suspend fun update(movie: Movie) {
         movies = movies.filterNot { it.id == movie.id } + movie
     }
+}
+
+class FakeRemoteDataSource : RemoteDataSource {
+
+    var movies = defaultFakeMovies
+
+    override suspend fun getPopularMovies(apiKey: String, region: String) = movies
 }
 
