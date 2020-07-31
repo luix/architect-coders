55a1642416376b999bbf62b7e82e69f60be5f517
LuiX <luis.a.vivero@gmail.com>
2020-07-30 22:00:30 -0700

    Start implementation in MainIntegrationTests class

--- app/src/test/java/mx/luix/mymovies/ui/main/MainIntegrationTests.kt
+++ app/src/test/java/mx/luix/mymovies/ui/main/MainIntegrationTests.kt
@@ -1,9 +1,37 @@
 package mx.luix.mymovies.ui.main
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import mx.luix.mymovies.ui.initMockedDi
+import mx.luix.mymovies.usecases.GetPopularMovies
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
-class MainIntegrationTests {
+class MainIntegrationTests : AutoCloseKoinTest() {
 
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var observer: Observer<MainViewModel.UiModel>
+
+    private lateinit var vm: MainViewModel
+
+    @Before
+    fun setUp() {
+        val vmModule = module {
+            factory { MainViewModel(get(), get()) }
+            factory { GetPopularMovies(get()) }
+        }
+
+        initMockedDi(vmModule)
+        vm = get()
+    }
 }
