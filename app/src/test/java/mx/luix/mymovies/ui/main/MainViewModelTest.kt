1578f8f35f6edfe7a602063960545ccc641ec849
LuiX <luis.a.vivero@gmail.com>
2020-07-30 22:00:52 -0700

    Start implementation in MainViewModelTest class

--- app/src/test/java/mx/luix/mymovies/ui/main/MainViewModelTest.kt
+++ app/src/test/java/mx/luix/mymovies/ui/main/MainViewModelTest.kt
@@ -1,9 +1,31 @@
 package mx.luix.mymovies.ui.main
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import kotlinx.coroutines.Dispatchers
+import mx.luix.mymovies.usecases.GetPopularMovies
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class MainViewModelTest {
-    
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var getPopularMovies: GetPopularMovies
+
+    @Mock
+    lateinit var observer: Observer<MainViewModel.UiModel>
+
+    private lateinit var vm: MainViewModel
+
+    @Before
+    fun setUp() {
+        vm = MainViewModel(getPopularMovies, Dispatchers.Unconfined)
+    }
 }
