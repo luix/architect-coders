eaad7aa8d6ee9dc5d17acd8a0aea6cd657d20c56
LuiX <luis.a.vivero@gmail.com>
2020-07-30 22:18:43 -0700

    Add a test to verify that data is loaded from data source when available in MainIntegrationTests

--- app/src/test/java/mx/luix/mymovies/ui/main/MainIntegrationTests.kt
+++ app/src/test/java/mx/luix/mymovies/ui/main/MainIntegrationTests.kt
@@ -3,6 +3,9 @@ package mx.luix.mymovies.ui.main
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
 import com.nhaarman.mockitokotlin2.verify
+import mx.luix.mymovies.data.source.LocalDataSource
+import mx.luix.mymovies.testshared.mockedMovie
+import mx.luix.mymovies.ui.FakeLocalDataSource
 import mx.luix.mymovies.ui.defaultFakeMovies
 import mx.luix.mymovies.ui.initMockedDi
 import mx.luix.mymovies.ui.main.MainViewModel.UiModel
@@ -46,5 +49,17 @@ class MainIntegrationTests : AutoCloseKoinTest() {
         vm.onCoarsePermissionRequested()
 
         verify(observer).onChanged(UiModel.Content(defaultFakeMovies))
+    }
+
+    @Test
+    fun `data is loaded from local source when available`() {
+        val fakeLocalMovies = listOf(mockedMovie.copy(10), mockedMovie.copy(11))
+        val localDataSource = get<LocalDataSource>() as FakeLocalDataSource
+        localDataSource.movies = fakeLocalMovies
+        vm.model.observeForever(observer)
+
+        vm.onCoarsePermissionRequested()
+
+        verify(observer).onChanged(UiModel.Content(fakeLocalMovies))
     }
 }
