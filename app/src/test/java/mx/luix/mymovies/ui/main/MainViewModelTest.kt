491233accbd2aa2611ef5ccc2fa67786d7a778b0
LuiX <luis.a.vivero@gmail.com>
2020-07-30 22:05:13 -0700

    Add a test that is observing LiveData launches location permission resquest

--- app/src/test/java/mx/luix/mymovies/ui/main/MainViewModelTest.kt
+++ app/src/test/java/mx/luix/mymovies/ui/main/MainViewModelTest.kt
@@ -2,10 +2,13 @@ package mx.luix.mymovies.ui.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.verify
 import kotlinx.coroutines.Dispatchers
 import mx.luix.mymovies.usecases.GetPopularMovies
+import mx.luix.mymovies.ui.main.MainViewModel.UiModel
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
@@ -20,12 +23,20 @@ class MainViewModelTest {
     lateinit var getPopularMovies: GetPopularMovies
 
     @Mock
-    lateinit var observer: Observer<MainViewModel.UiModel>
+    lateinit var observer: Observer<UiModel>
 
     private lateinit var vm: MainViewModel
 
     @Before
     fun setUp() {
         vm = MainViewModel(getPopularMovies, Dispatchers.Unconfined)
+    }
+
+    @Test
+    fun `observing LiveData launches location permission request`() {
+
+        vm.model.observeForever(observer)
+
+        verify(observer).onChanged(UiModel.RequestLocationPermission)
     }
 }
